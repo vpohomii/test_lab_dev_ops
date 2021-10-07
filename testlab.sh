14003c4a45872f46e3530b5dda84dc07b964da8d
vpohomii <92032080+vpohomii@users.noreply.github.com>
2021-10-07 17:41:09 +0300

    Create testlab.sh
    first version

---
+++ testlab.sh
@@ -0,0 +1,12 @@
+#This line need to be changed by Мария.
+#Feel free to write something.
+#And this line is also for you.
+#Line 4-6 for Михаил.
+#
+#
+#Line 7-9 for Андрій
+#
+#
+# And last three lines i'll fill by myself.
+#
+#
